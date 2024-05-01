427a7319eee90e6f47c3c9a75dba50a54b4b7b37
lucoiso <lucas.oliveiravb@gmail.com>
2024-05-01 22:07:13 +0100

    update .bat

--- config_win64_release.bat
+++ config_win64_release.bat
@@ -1,4 +1,12 @@
-@call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+@echo off
+SETLOCAL EnableDelayedExpansion
+
+call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+
+PowerShell -Command "& {Get-ChildItem build/* -Exclude Debug,Release | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue}"
+
 conan install . --build=missing --profile=Profiles/WinRel
 cmake --preset conan-release
 cmake --build build --target install --preset conan-release
+
+ENDLOCAL
